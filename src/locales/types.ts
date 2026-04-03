4ee34e8d088767bc7d32fac8beb86390b16383c7
AbdulLatef2380 <xwenbrr847@gmail.com>
2026-04-04 00:45:18 +0200

    Add AvailableLanguage type for supported languages

--- src/locales/types.ts
+++ src/locales/types.ts
@@ -1,3 +1,15 @@
 import en from './en.json';
 
 export type Translations = typeof en;
+
+export type AvailableLanguage =
+  | 'en'
+  | 'ar'
+  | 'fa'
+  | 'he'
+  | 'id'
+  | 'ja'
+  | 'ko'
+  | 'ms'
+  | 'ru'
+  | 'zh';
